f82aa79041358819077d9d14b08169c3c9a6e1a8
niadulMunna <niadulislammunna@gmail.com>
2025-03-12 23:52:28 +0600

    feat: add filter method

--- components/filter-items.jsx
+++ components/filter-items.jsx
@@ -19,20 +19,24 @@ const FilterItems = () => {
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
-    console.log(searchTerm);
   };
+  const fruitsDataFiltered = fruitsData.filter((fruit) => {
+    return fruit.toLowerCase().includes(searchTerm.toLowerCase());
+  });
   return (
     <div className="flex flex-col items-center justify-center w-full mt-10">
-      <input
-        type="text"
-        placeholder="Search here.."
-        className="border border-gray-400 px-2 outline-0"
-        onChange={handleInputChange}
-      />
-      <div className="mt-2">
-        {fruitsData.map((fruits) => {
-          return <p className="mb-2">{fruits}</p>;
-        })}
+      <div className="bg-neutral-200 p-5">
+        <input
+          type="text"
+          placeholder="Search here.."
+          className="border border-gray-400 px-2 outline-0"
+          onChange={handleInputChange}
+        />
+        <div className="mt-2 ">
+          {fruitsDataFiltered.map((fruits) => {
+            return <p className="mb-2 even:bg-gray-300">{fruits}</p>;
+          })}
+        </div>
       </div>
     </div>
   );
